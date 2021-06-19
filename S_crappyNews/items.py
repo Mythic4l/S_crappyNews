1ad24a0a122fc4c76f96816c7d0ed822e5725091
Anarch1st <ouzegza@gmail.com>
2021-06-19 05:51:45 +0100

    Changing the default values

--- S_crappyNews/items.py
+++ S_crappyNews/items.py
@@ -3,10 +3,10 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class SCrappynewsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class SpiderItems(Item):
+    title = Field()
+    body = Field()
+    url = Field()
--- S_crappyNews/pipelines.py
+++ S_crappyNews/pipelines.py
@@ -8,6 +8,49 @@
 from itemadapter import ItemAdapter
 
 
-class SCrappynewsPipeline:
+import pymongo
+from ssl import CERT_NONE
+from hashlib import sha256
+from scrapy.exceptions import DropItem
+
+
+class MongoPipeline(object):
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, bbcspider):
+        return cls(
+            mongo_uri=bbcspider.settings.get("MONGO_URI"),
+            mongo_db=bbcspider.settings.get("MONGO_DATABASE")
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri, ssl_cert_reqs=CERT_NONE)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        if len(item["title"]) == 0:
+            raise DropItem("Title is empty")
+
+        if item["body"] is None:
+            raise DropItem("Body is empty")
+
+        if type(item["title"]) == list:
+            item["title"] = item["title"][0]
+
+        items = dict(item)
+        items["checksum"] = sha256(item["body"]).hexdigest()
+
+        # drop item if document exist
+        query = {"checksum": items["checksum"]}
+        if self.db.news.find(query).limit(1).count() > 0:
+            raise DropItem("News exist")
+
+        self.db.news.insert_one(items)
         return item
